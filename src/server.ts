4f7e668e27874e8e670c2922ca738dfe4144e04b
Binhotti <vitorbinhotti@gmail.com>
2026-03-01 14:21:54 -0300

    adicinado correção de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,21 @@ app.post('/movies', async (req, res) => {
     const { title, genre_id, language_id, oscar_count, release_date } =
         req.body;
 
+    // case insentitive - se a busca for feita por john wick ou John wick ou JOHN WICK, o registro vai ser retornado na consulta
+
+    // case sensitive - se a busca por john wick e no banco estiver como John Wick, não vai ser retornado na consulta
+
     try {
+        const movieWithSameTitle = await prisma.movie.findFirst({
+            where: { title: { equals: title, mode: 'insensitive' } },
+        });
+
+        if (movieWithSameTitle) {
+            return res.status(409).send({
+                message: 'Já existe um filme cadastrado com esse título',
+            });
+        }
+
         await prisma.movie.create({
             data: {
                 title,
@@ -35,7 +49,7 @@ app.post('/movies', async (req, res) => {
                 release_date: new Date(release_date),
             },
         });
-    } catch(error) {
+    } catch (error) {
         console.error('Erro ao cadastrar filme:', error);
         return res.status(500).send({ message: 'Falha ao cadastrar um filme' });
     }
